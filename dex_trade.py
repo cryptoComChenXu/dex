f4c64ddd29cc60370f38d78aa201b10f473c9a4a
cryptoComChenXu <chen.xu@crypto.com>
2022-05-30 12:58:00 +0800

    mod

--- dex_trade.py
+++ dex_trade.py
@@ -26,3 +26,61 @@ q1 = 'trade_table = loadTable("dfs://tick_trade", "trades")'
 q2 = 'depth_table = loadTable("dfs://tick_depth", "depths")'
 s.run(q1)
 s.run(q2)
+
+start_date, end_date = "2022-03-01", "2022-05-25"
+
+with open("symbols.json", 'r') as f:
+    symbols = json.load(f)
+
+crossing_pairs = set([
+    "USDCUSDT", 
+    "DAIUSDT",
+    "CROUSDT",
+    "CROUSDC",
+    "BNBUSDT",
+    "BUSDUSDT",
+])
+
+wrapped = {
+    "BTC": "WBTC",
+    "ETH": "WETH",
+    "CRO": "WCRO",
+}
+
+binance_symbols = set([
+    "BUSD",
+    "BNB",
+])
+
+coins = set([
+    "ADA", "ALI", "ALICE", "ATOM", "AVAX", "BIFI", "BNB", "BTC", "C98",
+    "CAKE", "CHR", "CRO", "DAI", "DOT", "DOGE", "ELON", "ETH", "LINK",
+    "LTC", "MASK", "MATIC", "MMF", "SOL", "SPS", "TONIC", "TRX", "TSUD",
+    "UNI", "USDC", "USDT", "VVS", "WBTC", "WCRO", "WETH", "WOO", "XRP",
+])
+
+gas_fees = {
+    "UNS": 30,
+    "MMS": 0.5,
+    "VVS": 0.5,
+    "PCS": 5,
+}
+
+bps_exchanges = set([
+    "PCS"
+])
+
+delays = {
+    "UNS": 15,
+    "MMS": 6,
+    "VVS": 6,
+    "PCS": 3,
+}
+
+starts, ends = [], []
+start = datetime.strptime(f"{start_date} 00:00:00", "%Y-%m-%d %H:%M:%S")
+end = datetime.strptime(f"{end_date} 00:00:00", "%Y-%m-%d %H:%M:%S")
+while start < end:
+    starts.append(start.strftime("%Y.%m.%dT%H:%M:%S"))
+    start += timedelta(days=1)
+    ends.append(start.strftime("%Y.%m.%dT%H:%M:%S"))
